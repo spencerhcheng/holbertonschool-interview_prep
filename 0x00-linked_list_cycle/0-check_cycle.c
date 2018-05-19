af3e61b8d7f0d1a6bf4d740d2ffe8be78365b3ae
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-19 00:07:21 +0000

    0-check_cycle: initial commit

---
+++ 0x00-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * check_cycle - A function that checks whether
+ * a linked list has a cycle or not
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	slow = list;
+	fast = list;
+
+	short int first_flag = 0;
+
+	while (slow != NULL || fast != NULL)
+	{
+		if (slow == fast && first_flag == 1)
+			return (1);
+
+		first_flag = 1;
+
+		slow = slow->next;
+		if (fast->next->next != NULL)
+			fast = fast->next->next;
+		else
+			return (0);
+	}
+	return (0);
+}
